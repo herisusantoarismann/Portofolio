ac6d5d31a5f4c217dc0295df3c93eb3258989bff
Heri Susanto Arisman <eriparigi12@gmail.com>
2020-09-03 15:56:36 +0700

    Updates

---
+++ precache-manifest.330c576679f9df85ada86591fadad056.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0478fa08a989c8907549362727f20e",
+    "url": "/Portofolio/index.html"
+  },
+  {
+    "revision": "02186546a6f8ae9acb52",
+    "url": "/Portofolio/static/css/main.abc543fe.chunk.css"
+  },
+  {
+    "revision": "e5ba4b88fb7e511efb33",
+    "url": "/Portofolio/static/js/2.dfd2601b.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portofolio/static/js/2.dfd2601b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02186546a6f8ae9acb52",
+    "url": "/Portofolio/static/js/main.964212f4.chunk.js"
+  },
+  {
+    "revision": "452c98632515d64f6927",
+    "url": "/Portofolio/static/js/runtime-main.c6c91b76.js"
+  },
+  {
+    "revision": "b7548584546bc9ef05d9c795051c481a",
+    "url": "/Portofolio/static/media/foto.b7548584.jpg"
+  },
+  {
+    "revision": "8c69eed65bf47f1997f6e07fd3ce8137",
+    "url": "/Portofolio/static/media/project1.8c69eed6.png"
+  }
+]);
